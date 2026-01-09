bc111851f9ac7a340516d83d8bd2f4474555190a
Chaudhry Ali <chaudhryali2285@gmail.com>
2026-01-10 01:16:32 +0500

    Done

--- admin/src/resources/apiAssets.js
+++ admin/src/resources/apiAssets.js
@@ -1,8 +1,8 @@
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL + "/api/v1"
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL 
 
-export const ADD_SERVICE = BASE_URL + "/add"
-export const LIST_SERVICE = BASE_URL + "/list"
-export const ORDER_URI = BASE_URL + "/orderlist"
-export const DELETE_SERVICE = BASE_URL + "/remove"
-export const UPDATE_STATUS = BASE_URL + "/status"
-export const ADMIN_LOGIN_URL = BASE_URL + "/admin";
+export const ADD_SERVICE = BASE_URL + "/api/v1/add"
+export const LIST_SERVICE = BASE_URL + "/api/v1/list"
+export const ORDER_URI = BASE_URL + "/api/v1/orderlist"
+export const DELETE_SERVICE = BASE_URL + "/api/v1/remove"
+export const UPDATE_STATUS = BASE_URL + "/api/v1/status"
+export const ADMIN_LOGIN_URL = BASE_URL + "/api/v1/admin";
--- client/src/resources/api.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URI = import.meta.env.VITE_BACKEND_URL 
-export const ADMIN_URI = BASE_URI + "/api/v1/admin";
